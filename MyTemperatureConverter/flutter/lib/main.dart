004a5e323e1418e5dd3af0248b9bf6b41264e4bb
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-02-06 09:58:48 +0700

    add two way converter - Flutter

--- MyTemperatureConverter/flutter/lib/main.dart
+++ MyTemperatureConverter/flutter/lib/main.dart
@@ -31,6 +31,7 @@ class MyHomePage extends StatelessWidget {
         children: const <Widget>[
           StatefulTemperatureInput(),
           ConverterApp(),
+          TwoWayConverterApp(),
         ],
       ),
     );
@@ -41,6 +42,12 @@ String convertToFahrenheit(String celsius) {
   final dCelsius = double.tryParse(celsius) ?? 0;
   final dFahrenheit = (dCelsius * 9 / 5) + 32;
   return dFahrenheit.toString();
+}
+
+String convertToCelsius(String fahrenheit) {
+  final dFahrenheit = double.tryParse(fahrenheit) ?? 0;
+  final dCelsius = (dFahrenheit - 32) * 5 / 9;
+  return dCelsius.toString();
 }
 
 class StatefulTemperatureInput extends StatefulWidget {
@@ -137,6 +144,89 @@ class StatelessTemperatureInput extends StatelessWidget {
             onChanged: (value) => onValueChange(value),
           ),
           Text(temperatureFahrenheit(output))
+        ],
+      ),
+    );
+  }
+}
+
+enum Scale {
+  celsius("Celcius"),
+  fahrenheit("Fahrenheit");
+
+  final String scaleName;
+  const Scale(this.scaleName);
+}
+
+class GeneralTemperaturInput extends StatelessWidget {
+  final TextEditingController controller;
+  final Scale scale;
+  final Function(String newInput) onValueChange;
+
+  const GeneralTemperaturInput({
+    super.key,
+    required this.controller,
+    required this.scale,
+    required this.onValueChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          hintText: enterTemperature(scale.scaleName),
+          border: const OutlineInputBorder(),
+        ),
+        onChanged: (value) => onValueChange(value),
+      ),
+    );
+  }
+}
+
+class TwoWayConverterApp extends StatefulWidget {
+  const TwoWayConverterApp({super.key});
+
+  @override
+  State<TwoWayConverterApp> createState() => _TwoWayConverterAppState();
+}
+
+class _TwoWayConverterAppState extends State<TwoWayConverterApp> {
+  String celsius = "";
+  String fahrenheit = "";
+  final TextEditingController cCelcius = TextEditingController();
+  final TextEditingController cFahrenheit = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            twoWayConverter,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          GeneralTemperaturInput(
+            controller: cCelcius,
+            scale: Scale.celsius,
+            onValueChange: (newInput) {
+              fahrenheit = convertToFahrenheit(newInput);
+              cFahrenheit.text = fahrenheit;
+            },
+          ),
+          GeneralTemperaturInput(
+            controller: cFahrenheit,
+            scale: Scale.fahrenheit,
+            onValueChange: (newInput) {
+              celsius = convertToCelsius(newInput);
+              cCelcius.text = celsius;
+            },
+          ),
         ],
       ),
     );
