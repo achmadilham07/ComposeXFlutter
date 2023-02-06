22609bd24114981e687e707b5bbb76cc845fe215
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-02-06 09:15:18 +0700

    add stateful converter - Flutter

---
+++ MyTemperatureConverter/flutter/lib/utils/string.dart
@@ -0,0 +1,7 @@
+const String statefulConverter = "Stateful Converter";
+const String statelessConverter = "Stateless Converter";
+const String twoWayConverter = "Two Way Converter";
+const String enterCelsius = "Enter temperature in Celsius";
+String temperatureFahrenheit(String value) =>
+    "Temperature in Fahrenheit : $value";
+String enterTemperature(String value) => "Enter temperature in $value";
